8598c8c3a6ee7f1e1fc9eb13de9bc6930872795a
samlingbcba <112557952+samlingbcba@users.noreply.github.com>
2024-10-14 14:16:20 -0600

    Initial commit

---
+++ config.py
@@ -0,0 +1,19 @@
+market_operators_config = {
+    'optimism': {
+        '0xD74A1f6B44395cf8c4833df5Bc965C6C2b567476': {'collateral_name': 'WETH', 'collateral_decimals': 18},
+        '0xC82B4C656BA6Aa4A2eF6bfe6b511d206C93b405b': {'collateral_name': 'WBTC', 'collateral_decimals': 8},
+        '0xfC6Ec1F94f2fFCe0f0BcB79592D765abD3E1BAEf': {'collateral_name': 'WSTETH', 'collateral_decimals': 18},
+        '0x13aa7dBB49d414A321b403EabB1B4231e61C7b29': {'collateral_name': 'OP', 'collateral_decimals': 18},
+        '0x7e0242FCAA2d4844C6fF0769fac9c9cF5f8DE2d6': {'collateral_name': 'VELO', 'collateral_decimals': 18}
+    },
+
+    'arbitrum': {
+        '0xe38FB572099A8Fdb51e0929cb2B439D0479fC43E': {'collateral_name': 'WETH', 'collateral_decimals': 18},
+        '0xB745f12eCF271484c79D3999cA12164Fe1c4e5f9': {'collateral_name': 'WBTC', 'collateral_decimals': 8},
+        '0xEC70Ac48D2cc382987a176f64fe74d77d010F9D1': {'collateral_name': 'ARB', 'collateral_decimals': 18},
+        '0xE927e8B43Da90f017b146Eff5f99515372630DD1': {'collateral_name': 'WSTETH', 'collateral_decimals': 18},
+        '0xa8ED217624218a4c65e6d577A26D7810E2f8f790': {'collateral_name': 'GMX', 'collateral_decimals': 18},
+        '0xE304eF44F4240E44d0A8E954c22e5007a93a4378': {'collateral_name': 'RDNT', 'collateral_decimals': 18},
+        '0xFF75fa72bbc5DB02FceB948901614A1155925592': {'collateral_name': 'PENDLE', 'collateral_decimals': 18}
+    }
+}
---
+++ main.py
@@ -0,0 +1,189 @@
+import os
+import asyncio
+from dotenv import load_dotenv
+
+from rich import print
+from rich.traceback import install
+
+import hikari
+
+from web3 import Web3, AsyncWeb3, WebSocketProvider
+
+from eth_abi import decode
+
+from config import market_operators_config
+
+load_dotenv()
+install()
+
+DISCORD_BOT_TOKEN = os.environ['DISCORD_BOT_TOKEN']
+CHANNEL_ID = os.environ['CHANNEL_ID']
+
+ARBITRUM_RPC_URL = os.environ['ARBITRUM_RPC_URL']
+OPTIMISM_RPC_URL = os.environ['OPTIMISM_RPC_URL']
+
+MAIN_CONTROLLER = '0x1337F001E280420EcCe9E7B934Fa07D67fdb62CD'
+
+class EventListener:
+    """
+    Listens for events on the Optimism and Arbitrum blockchains and handles them accordingly.
+    
+    The `EventListener` class is responsible for subscribing to various events on the Optimism and Arbitrum blockchains, such as CreateLoan, CloseLoan, LiquidateLoan, and AdjustLoan. When these events are detected, the class calls the corresponding event handler methods to process the event data and send a message to a Discord channel.
+    
+    The `subscribe_to_events` method is the main entry point for the event listener, where it sets up the event filters and starts listening for events. The event handler methods, such as `handle_create_loan_event`, `handle_close_loan_event`, `handle_liquidate_loan_event`, and `handle_adjust_loan_event`, are responsible for decoding the event data, converting the values to appropriate decimal formats, and sending the message to the Discord channel using the `send_message_to_channel` function.
+    """
+
+    def __init__(self, chain: str, async_w3):
+        self.chain = chain
+        self.async_w3 = async_w3
+        self.market_operators = market_operators_config[self.chain]
+
+    async def subscribe_to_events(self):
+        async for w3 in self.async_w3:
+            print(
+                f"{self.chain.upper()} Is connected to websocket: {await w3.is_connected()}"
+            )
+
+            TOPICS = {
+                'CreateLoan': '0xbf2742c8e657897c9f047c065e83679fdea6e8bf1a460402c3c922f800b74bf1',
+                'AdjustLoan': '0x79be6dfcb3a3568b21afc806c66bcd67098197716d17923c3189da0c7973f826',
+                'LiquidateLoan': '0x9ef0a399defbe09357ef7431cb1cab06dd5ede64767faa82a286b5cbda5eeaf5',
+                'CloseLoan': '0xabba776d3d0b8a6980d7277a9d4c2b2d7d9ce50e6d0deac46dd8a52437869ed9'
+            }
+            create_loan_filter = {
+                "address": [MAIN_CONTROLLER],
+                "topics": [TOPICS["CreateLoan"]],
+            }
+
+            close_loan_filter = {
+                "address": [MAIN_CONTROLLER],
+                "topics": [TOPICS["CloseLoan"]],
+            }
+
+            liquidate_loan_filter = {
+                "address": [MAIN_CONTROLLER],
+                "topics": [TOPICS["LiquidateLoan"]],
+            }
+
+            adjust_loan_filter = {
+                "address": [MAIN_CONTROLLER],
+                "topics": [TOPICS["AdjustLoan"]],
+            }
+
+            await w3.eth.subscribe("logs", create_loan_filter)
+            await w3.eth.subscribe("logs", close_loan_filter)
+            await w3.eth.subscribe("logs", liquidate_loan_filter)
+            await w3.eth.subscribe("logs", adjust_loan_filter)
+
+            async for event in w3.socket.process_subscriptions():
+                print(event)
+                topic0 = Web3.to_hex(event['result']['topics'][0])
+
+                if topic0 == TOPICS["CreateLoan"]:
+                    await self.handle_create_loan_event(event)
+                
+                elif topic0 == TOPICS["CloseLoan"]:
+                    await self.handle_close_loan_event(event)
+
+                elif topic0 == TOPICS["LiquidateLoan"]:
+                    await self.handle_liquidate_loan_event(event)
+
+                elif topic0 == TOPICS["AdjustLoan"]:
+                    await self.handle_adjust_loan_event(event)
+
+    async def handle_create_loan_event(self, event):
+        # Get market operator address from event
+        market_operator = decode(['address'], event['result']['topics'][1])[0] # Decode from bytes32
+        market_operator = Web3.to_checksum_address(market_operator) # Convert to checksum address
+        collateral_name = self.market_operators[market_operator]['collateral_name']
+
+        # Decode data
+        coll_amount, debt_amount = decode(['uint256', 'uint256'], event['result']['data'])
+        
+        # Convert values to decimals
+        coll_amount_dec = round(coll_amount / 10 ** self.market_operators[market_operator]['collateral_decimals'], 6)
+        debt_amount_dec = round(debt_amount / 10 ** 18, 2) # $MONEY has 18 decimals
+        
+        await send_message_to_channel(f">>> ### New loan created on {self.chain.upper()}: \n{collateral_name} deposited: __{coll_amount_dec}__ \n$MONEY minted: __{debt_amount_dec}__")
+
+    async def handle_close_loan_event(self, event):
+        # Get market operator address from event
+        market_operator = decode(['address'], event['result']['topics'][1])[0] # Decode from bytes32
+        market_operator = Web3.to_checksum_address(market_operator) # Convert to checksum address
+        collateral_name = self.market_operators[market_operator]['collateral_name']
+
+        # Decode data
+        coll_withdrawn, debt_withdrawn, debt_repaid = decode(['uint256', 'uint256', 'uint256'], event['result']['data'])
+        
+        # Convert values to decimals
+        coll_withdrawn_amount_dec = round(coll_withdrawn / 10 ** self.market_operators[market_operator]['collateral_decimals'], 6)
+        debt_withdrawn_amount_dec = round(debt_withdrawn / 10 ** 18, 2) # $MONEY has 18 decimals
+        debt_repaid_amount_dec = round(debt_repaid / 10 ** 18, 2) # $MONEY has 18 decimals
+        
+        await send_message_to_channel(f">>> ### Loan closed on {self.chain.upper()}: \n{collateral_name} withdrawn: __{coll_withdrawn_amount_dec}__ \n$MONEY withdrawn: __{debt_withdrawn_amount_dec}__ \n$MONEY repaid: __{debt_repaid_amount_dec}__")
+
+    async def handle_liquidate_loan_event(self, event):
+        # Get market operator address from event
+        market_operator = decode(['address'], event['result']['topics'][1])[0] # Decode from bytes32
+        market_operator = Web3.to_checksum_address(market_operator) # Convert to checksum address
+        collateral_name = self.market_operators[market_operator]['collateral_name']
+
+        # Decode data
+        coll_received, debt_received, debt_repaid = decode(['uint256', 'uint256', 'uint256'], event['result']['data'])
+        
+        # Convert values to decimals
+        coll_received_amount_dec = round(coll_received / 10 ** self.market_operators[market_operator]['collateral_decimals'], 6)
+        debt_received_amount_dec = round(debt_received / 10 ** 18, 2) # $MONEY has 18 decimals
+        debt_repaid_amount_dec = round(debt_repaid / 10 ** 18, 2) # $MONEY has 18 decimals
+        
+        await send_message_to_channel(f">>> ### Loan liquidated on {self.chain.upper()}: \n{collateral_name} received: __{coll_received_amount_dec}__ \n$MONEY received: __{debt_received_amount_dec}__ \n$MONEY repaid: __{debt_repaid_amount_dec}__")
+
+    async def handle_adjust_loan_event(self, event):
+        # Get market operator address from event
+        market_operator = decode(['address'], event['result']['topics'][1])[0] # Decode from bytes32
+        market_operator = Web3.to_checksum_address(market_operator) # Convert to checksum address
+        collateral_name = self.market_operators[market_operator]['collateral_name']
+
+        # Decode data
+        coll_adjustment, debt_adjustment = decode(['int256', 'int256'], event['result']['data'])
+        
+        # Convert values to decimals
+        coll_adjustment_amount_dec = round(coll_adjustment / 10 ** self.market_operators[market_operator]['collateral_decimals'], 6)
+        debt_adjustment_amount_dec = round(debt_adjustment / 10 ** 18, 2) # $MONEY has 18 decimals
+        
+        await send_message_to_channel(f">>> ### Loan adjustment on {self.chain.upper()}: \n{collateral_name} change: __{coll_adjustment_amount_dec}__ \n$MONEY change: __{debt_adjustment_amount_dec}__")
+
+async def send_message_to_channel(message):
+    rest = hikari.RESTApp()
+
+    await rest.start()
+
+    # We acquire a client with a given token. This allows one REST app instance
+    # with one internal connection pool to be reused.
+    async with rest.acquire(DISCORD_BOT_TOKEN, "Bot") as client:
+        await client.create_message(CHANNEL_ID, message)
+
+    await rest.close()
+    
+async def main():
+    optimism_async_w3 = AsyncWeb3(WebSocketProvider(OPTIMISM_RPC_URL))
+    arbitrum_async_w3 = AsyncWeb3(WebSocketProvider(ARBITRUM_RPC_URL))
+
+    optimism_listener = EventListener('optimism', optimism_async_w3)
+    arbitrum_listener = EventListener('arbitrum', arbitrum_async_w3)
+
+    tasks = [
+        optimism_listener.subscribe_to_events(),
+        arbitrum_listener.subscribe_to_events()
+    ]
+
+    await asyncio.gather(*tasks)
+
+if __name__ == "__main__":
+    while True:
+        try:
+            asyncio.run(main())
+
+        except Exception as e:
+            print(f'Error: \n{e}')
+            continue
